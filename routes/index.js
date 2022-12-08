36116590c0e592f50d135463d374f4b2b0c61da4
Ryan P <rdpinkstaff@gmail.com>
2022-12-07 17:13:15 -0700

    Working message board and working on styling

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,21 @@ const messages = [
 var express = require('express');
 var router = express.Router();
 
+/* POST data from form */
+router.post('/new', function(req, res, next) {
+  const newPost = req.body;
+  if(newPost.user && newPost.message) {
+    const post = {
+      text: newPost.message,
+      user: newPost.user,
+      added: new Date(),
+    }
+    messages.push(post);
+  } 
+  res.redirect('/')
+
+})
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Mini Messageboard', messages });
